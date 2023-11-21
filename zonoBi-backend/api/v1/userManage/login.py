7cf9ef8d2765853569ca12b4ec6ece9b64c342d4
zo-no <2742160682@qq.com>
2023-11-21 23:17:36 +0800

    找到的header不加入头的问题根源

--- zonoBi-backend/api/v1/userManage/login.py
+++ zonoBi-backend/api/v1/userManage/login.py
@@ -7,13 +7,13 @@
 from datetime import datetime, timedelta
 from typing import Union
 
-from fastapi import APIRouter, Depends, HTTPException,status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 
 # ---------------------数据库有关的导入------------------------------------------
 from sqlalchemy.orm import Session
 
-from database.sqlite import engine,Base,SessionLocal
+from database.sqlite import engine, Base, SessionLocal
 
 from crud import crud
 from models import base
@@ -29,13 +29,14 @@ loginUP = APIRouter()
 Base.metadata.create_all(bind=engine)
 
 # ------------------------初始化设置---------------------------------------
-SECRET_KEY = "efb8d310a2e46e859a8e2f196ad25ff41916c90828999d64d50699f4f6cae93c"#TODO 加入配置中
-ALGORITHM = "HS256"#算法变量
-ACCESS_TOKEN_EXPIRE_MINUTES = 30#过期时间
+SECRET_KEY = "efb8d310a2e46e859a8e2f196ad25ff41916c90828999d64d50699f4f6cae93c"  # TODO 加入配置中
+ALGORITHM = "HS256"  # 算法变量
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 过期时间
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")#使用bcrypt算法对密码加密
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")#用户校验地址
+pwd_context = CryptContext(
+    schemes=["bcrypt"], deprecated="auto")  # 使用bcrypt算法对密码加密
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")  # 用户校验地址
 
 
 # ---------------------------------------------------------------
@@ -53,7 +54,9 @@ def get_db():
         db.close()
 
 # -----------------------函数区----------------------------------------
-def verify_password(plain_password:str, hashed_password:str) -> bool:
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     """
     @description  :
     校验密码
@@ -63,8 +66,9 @@ def verify_password(plain_password:str, hashed_password:str) -> bool:
     bool
     """
     return pwd_context.verify(plain_password, hashed_password)
-    
-def authenticate_user(username: str, password: str,db):
+
+
+def authenticate_user(username: str, password: str, db):
     """
     @description  :
     调用数据库对数据进行比对
@@ -76,10 +80,12 @@ def authenticate_user(username: str, password: str,db):
     user = crud.get_user_by_username(db, username)
     if not user:
         return False
-    db_user=db.query(base.User).filter(base.User.username== username).first()
+    db_user = db.query(base.User).filter(
+        base.User.username == username).first()
     if not verify_password(password, db_user.hashed_password):
         return False
     return user
+
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     """
@@ -92,16 +98,19 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     """
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta#加上过期时间
+        expire = datetime.utcnow() + expires_delta  # 加上过期时间
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)#TODO默认过期时间，集成到config中
+        expire = datetime.utcnow() + timedelta(minutes=15)  # TODO默认过期时间，集成到config中
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)#入值：字符、key、算法->出值：Token
+    # 入值：字符、key、算法->出值：Token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 # --------------------------接口调用-------------------------------------
-@loginUP.post("/token",response_model=schemas.Token,summary="获取Token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depends(get_db)):
+
+
+@loginUP.post("/token", response_model=schemas.Token, summary="获取Token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     """
     @description  :
     登录接口,并返回Token
@@ -110,12 +119,12 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depe
     @Returns  :
     access_token
     """
-    user = authenticate_user(form_data.username,form_data.password,db)
+    user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                             detail="账号或密码错误，或该用户不存在",
-                             headers={"WWW-Authenticate": "Bearer"},
-        )
+                            detail="账号或密码错误，或该用户不存在",
+                            headers={"WWW-Authenticate": "Bearer"},
+                            )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": form_data.username}, expires_delta=access_token_expires
@@ -125,7 +134,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depe
 # -----------------------------调用接口取信息----------------------------------
 
 
-async def get_current_user(db:Session=Depends(get_db),token: str = Depends(oauth2_scheme)):
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     """
     @description  :
     校验Token，验证用户，并获取当前用户
@@ -134,26 +143,29 @@ async def get_current_user(db:Session=Depends(get_db),token: str = Depends(oauth
     @Returns  :
     -------
     """
-    #TODO 获取用户量可以设置,调整对应用户才能使用对应数据
+    # TODO 获取用户量可以设置,调整对应用户才能使用对应数据
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="无法验证凭据，可能是登录过期,或者用户已注销",#TODO 日后完善
+        detail="无法验证凭据，可能是登录过期,或者用户已注销",  # TODO 日后完善
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])#与encode反过来
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[
+                             ALGORITHM])  # 与encode反过来
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        #XXX获取所有用户 token_data = crud.get_users(db, skip=0, limit=100)
+        # XXX获取所有用户 token_data = crud.get_users(db, skip=0, limit=100)
         token_data = crud.get_user_by_username(db, username)
-        #TODO修改为只取对应用户
+        # TODO修改为只取对应用户
     except JWTError:
         raise credentials_exception
     return token_data
 
-#TODO post应该也有办法
+# TODO post应该也有办法
 # ---------------------------------------------------------------
+
+
 async def get_current_active_user(current_user: schemas.UserInDB = Depends(get_current_user)):
     """
     @description  :
@@ -164,6 +176,7 @@ async def get_current_active_user(current_user: schemas.UserInDB = Depends(get_c
     -------
     """
     return current_user
+
 
 @loginUP.get("/users/me")
 async def read_users_me(current_user: schemas.UserInDB = Depends(get_current_active_user)):
